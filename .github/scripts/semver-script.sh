52b1dd7d6a14b526a40100fac9e7b6be4cc839f1
John Murwin <john.murwin@gmail.com>
2021-12-07 11:38:48 -0500

    successfully reading version from version.txt

--- .github/scripts/semver-script.sh
+++ .github/scripts/semver-script.sh
@@ -9,12 +9,14 @@
 # required files to use to update version information
 VERSION_FILE="./.github/version.txt"
 
-VERSION=0.3.0
-
 # put the version info into the environment
 if [ -f "$VERSION_FILE" ]; then
-  export VERSION
+  while IFS='=' read -r key value; do eval ${key}=${value};  done < "$VERSION_FILE"
 else
   echo "Missing $VERSION_FILE"
   exit 1
 fi
+
+CURRENT_VERSION="$BREAKING_VERSION.$RELEASE_VERSION.$FEATURE_VERSION"
+
+export CURRENT_VERSION
