30f887304e43abe854208c302079cd5dd873768c
John Murwin <john.murwin@gmail.com>
2021-12-07 10:13:01 -0500

    Update to shell script for version tag output

--- .github/workflows/semver-script.sh
+++ .github/workflows/semver-script.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
 #   SemVer Script
 #   Uses: custom
 #   From: Timothy Yolt, John Murwin
 
 ## Is used to calculate the SemVer number based of current version, and outputs version for use in Github
 ## NOTE: This script should only run on develop or main!
+
+# required files to use to update version information
+
+export VERSION=0.3.0
