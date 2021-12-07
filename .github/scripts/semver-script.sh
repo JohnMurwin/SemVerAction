88c656ca5fac3ff6095453584bec574bb79015f5
John Murwin <john.murwin@gmail.com>
2021-12-07 12:40:36 -0500

    starting logic for calculating when to bump semver

--- .github/scripts/semver-script.sh
+++ .github/scripts/semver-script.sh
@@ -6,10 +6,16 @@
 ## Is used to calculate the SemVer number based of current version, and outputs version for use in Github
 ## NOTE: This script should only run on develop or main!
 
-# required files to use to update version information
+# REQUIRED files to use to update version information
 VERSION_FILE="./.github/version.txt"
+SONAR_FILE=""
 
-# put the version info into the environment
+if [ "$GITHUB_REF_NAME" == "" ]; then
+  echo "Unknown Github branch name, is the SemVer.yml Workflow setup correctly?"
+  exit 1
+fi
+
+# 1. Put the version info into the environment for calculation & potential manipulation
 if [ -f "$VERSION_FILE" ]; then
   while IFS='=' read -r key value; do eval ${key}=${value};  done < "$VERSION_FILE"
 else
@@ -17,6 +23,14 @@ else
   exit 1
 fi
 
+# 2. Store combination of values from VERSION_FILE into CURRENT_VERSION
 CURRENT_VERSION="$BREAKING_VERSION.$RELEASE_VERSION.$FEATURE_VERSION"
 
+# 3. IF NECCESSARY, Grab last tag_commit_sha for comparison check before verBump
+
 export CURRENT_VERSION
+
+
+  # X. Update VERSION_FILE
+
+  # X. Update SONAR_FILE
